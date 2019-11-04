ae80f34556d2f36fba1f92bebd228e6212cd730b
Martin Stanimirov <marto0799@gmail.com>
2019-11-04 07:33:17 +0200

    Update#13
    *Added Cowboy Snake

---
+++ SheriffWest/Assets/Scripts/Controllers/Characters/Enemies/Snake.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : EnemyController
+{
+
+    [Header("Custom Settings: ")]
+    public GameObject bloodParticle;
+
+    #region Private Vars
+
+    private Weapon gun;
+
+    #endregion
+
+    protected override void Awake()
+    {
+
+        base.Awake();
+        gun = GetComponentInChildren<Weapon>();
+
+    }
+
+    protected override void Start()
+    {
+
+        base.Start();
+
+        characterAnim.OnShoot += gun.Shoot;
+
+    }
+
+    public override void OnAttack(IDamageable target)
+    {
+
+        base.OnAttack(target);
+
+    }
+
+    public override void TakeDamage(int amount)
+    {
+
+        bloodParticle.SetActive(true);
+        base.TakeDamage(amount);
+
+    }
+
+    public override void OnDeath()
+    {
+
+        base.OnDeath();
+        GameController.instance.UnlockNextLevel();
+
+    }
+
+}
